a4d36af195f69b8d7fc07ac0c12e086424a2768f
Luis <luis_baltodano@ucne.edu.do>
2022-10-02 16:02:14 -0400

    API PRODUCCION GET POST

--- src/index.js
+++ src/index.js
@@ -1,16 +1,5 @@
-import express from "express"
-import articulosRoutes from './routes/articulos.routes.js'
-import indexRoutes from './routes/index.routes.js'
+import app from './app.js'
+import { PORT } from './config.js'
 
-
-const app = express()
-
-app.use(express.json())
-
-app.use(indexRoutes)
-app.use('/api', articulosRoutes)
-
-
-
-app.listen(3000)
-console.log("Servidor corriendo en el puerto 3000")
+app.listen(PORT)
+console.log('Servidor corriendo en el puerto',PORT)
